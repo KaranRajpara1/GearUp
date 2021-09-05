4c214aae3181d85e48fd2a2f0aa71213a3f2cb32
Karan Rajpara <karan.rajpara107598@marwadiuniversity.ac.in>
2021-09-06 02:33:59 +0530

    Intent added from smsbtn to recoverpage and recoverypage to change password

--- app/src/main/java/com/example/gear_up/forgot_password.kt
+++ app/src/main/java/com/example/gear_up/forgot_password.kt
@@ -1,11 +1,20 @@
 package com.example.gear_up
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class forgot_password : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        val smsBtn = findViewById<ImageButton>(R.id.SMSbutton)
+
+        smsBtn.setOnClickListener() {
+            val myIntent = Intent(this, recoverypage::class.java)
+            startActivity(myIntent)
+        }
     }
 }
--- app/src/main/java/com/example/gear_up/recoverypage.kt
+++ app/src/main/java/com/example/gear_up/recoverypage.kt
@@ -1,11 +1,20 @@
 package com.example.gear_up
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class recoverypage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recoverypage)
+
+        val nextBtn = findViewById<ImageButton>(R.id.next)
+
+        nextBtn.setOnClickListener() {
+            val myIntent = Intent(this, change_password::class.java)
+            startActivity(myIntent)
+        }
     }
 }
